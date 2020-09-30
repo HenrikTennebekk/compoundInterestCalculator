99150a9d9dca01618244af02269b5e616b6a6fa0
Henrik Tennebekk <henrik.ten@gmail.com>
2020-09-30 15:50:05 +0200

    Updating

--- compoundInterest.js
+++ compoundInterest.js
@@ -3,7 +3,7 @@ document.getElementById("calculate").onclick = function () {
     var a = document.getElementById("addition").value;
     var y = document.getElementById("years").value;
     var i = document.getElementById("interest").value;
-    var t = p * (1 + (i / 100) ^ y);
+    var t = p * (i / 100 + 1) ^ y;
 
     document.getElementById("total").innerHTML = "The total would be: " + t + " after " + y + " years";
 }
