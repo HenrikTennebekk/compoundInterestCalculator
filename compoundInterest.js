3ef230eedd392fface5a1753e6be5c94468fb1f8
Henrik Tennebekk <henrik.ten@gmail.com>
2020-10-01 16:33:19 +0200

    Bug fix

--- compoundInterest.js
+++ compoundInterest.js
@@ -7,10 +7,10 @@ document.getElementById("calculate").onclick = function () {
     var gt = g - ((g - p) * (0.22 * 1.44));
     var tax = document.getElementById("tax");
 
-    if (tax.checked = true) {
+    if (tax.checked == true) {
         document.getElementById("total").innerHTML = "The total would be: " + gt.toFixed(2) + " after " + y + " years";
     }
-    else if (tax.checked = false) {
+    else if (tax.checked == false) {
         document.getElementById("total").innerHTML = "The total would be: " + g.toFixed(2) + " after " + y + " years";
     }
 }
