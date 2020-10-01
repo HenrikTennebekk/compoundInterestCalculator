4aec80c6fd4ded7a03506f54f09d9714e78be37e
Henrik Tennebekk <henrik.ten@gmail.com>
2020-10-01 10:41:04 +0200

    Started adding tax feature

--- compoundInterest.js
+++ compoundInterest.js
@@ -3,7 +3,14 @@ document.getElementById("calculate").onclick = function () {
     var a = document.getElementById("addition").value;
     var y = document.getElementById("years").value;
     var i = document.getElementById("interest").value;
-    var t = Math.pow((i / 100 + 1), y) * p;
+    var g = Math.pow((i / 100 + 1), y) * p;
+    var gt = g - ((g - p) * (0.22 * 1.44));
+    var tax = document.getElementById("tax");
 
-    document.getElementById("total").innerHTML = "The total would be: " + t.toFixed(2) + " after " + y + " years";
+    if (tax.checked = true) {
+        document.getElementById("total").innerHTML = "The total would be: " + gt.toFixed(2) + " after " + y + " years";
+    }
+    else if (tax.checked = false) {
+        document.getElementById("total").innerHTML = "The total would be: " + g.toFixed(2) + " after " + y + " years";
+    }
 }
